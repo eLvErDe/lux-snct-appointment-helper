e8c4b6acfc237ee359805fa8e20a12bdf9053486
Adam Cécile (Le_Vert) <acecile@le-vert.net>
2019-04-17 08:28:53 +0200

    Improvme API middleware to drop stack logging for regular errors

--- api_middlewares.py
+++ api_middlewares.py
@@ -20,23 +20,29 @@ async def rest_error_middleware(_, handler, logger=None):
 
         except Exception as exc:  # pylint: disable=broad-except
 
-            # Log exception if have a logger
-            if isinstance(logger, logging.Logger):
-                logger.exception("Error handling request: %s: %s", exc.__class__.__name__, exc)
+            log_level = logging.ERROR
+            with_exception = True
 
             # Define status code according to exception type
-            if isinstance(exc, aiohttp.web.HTTPException):
+            if isinstance(exc, aiohttp.web_exceptions.HTTPException):
                 status = exc.status  # pylint: disable=no-member
                 message = exc.reason  # pylint: disable=no-member
+                log_level = logging.WARNING
+                with_exception = False
             elif isinstance(exc, AssertionError):
                 status = 400
                 message = str(exc)
+                log_level = logging.WARNING
+                with_exception = False
             else:
                 status = 500
                 message = "Internal Server Error"
 
-            rest_error = {"message": message, "status": status}
+            # Log exception if have a logger
+            if isinstance(logger, logging.Logger):
+                logger.log(log_level, "Error handling request: %s: %s" % (exc.__class__.__name__, exc), exc_info=with_exception)
 
+            rest_error = {"message": message, "status": status}
             response = aiohttp.web.json_response(rest_error, status=status)
 
         finally:
